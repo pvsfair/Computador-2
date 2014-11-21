92d91d47bfb294f20224d36b381b89e3bf505e58
pvsfair <pvsfair@gmail.com>
2014-11-20 21:04:49 -0300

    Primeiro Commit

---
+++ Computador2/src/computador/Perifericos/FoneDeOuvido.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package computador.Perifericos;
+
+import computador.Generalizacoes.Periferico;
+
+/**
+ *
+ * @author Paulo
+ */
+public class FoneDeOuvido implements Periferico{
+    
+}
---
+++ Computador2/src/computador/Programas/Notepad.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package computador.Programas;
+
+import computador.Generalizacoes.Programa;
+
+/**
+ *
+ * @author Paulo
+ */
+public class Notepad implements Programa{
+    
+}
